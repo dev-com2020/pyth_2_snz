c8e11ce1172da511768a9bf1f2b0d44b433faf9f
tomaszkaniecki <kwp@tlen.pl>
2023-06-23 10:54:41 +0200

    23.06 10:54

---
+++ pan.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+dane = pd.read_csv('pliki/AAPL_stock_price_example.csv', index_col='Date', parse_dates=True).assign(
+    close_price_trading=lambda x: pd.cut(
+        x.Close, bins=3, labels=['low', 'med', 'high']
+    )
+)
+# print(dane)
+# print(dane[224:226])
+# print(dane['Date'].is_unique)
+# print(dane['2019-07-01':'2019-09-01'])
+print(dane.first('1M'))
+print(dane.last('1M'))
